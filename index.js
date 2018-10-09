5a0cdff44a3c8c0c13dbef8411c3d1f765e7acbd
Ferdi Ramdhon Nizar <ferdi@axiatadigital.com>
2018-10-09 08:33:50 +0800

    add merchant

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+const { Merchant } = require('./sequelize')
+
+const app = express()
+app.use(bodyParser.json())
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+const port = 3000
+app.listen(port, () => {
+	console.log(`Running on pprt ${port}`)
+})
+
+// create a merchant
+app.post('/api/merchants', (req, res) => {
+	Merchant.create(req.body)
+		.then(merchant => res.json(merchant))
+})
+
+// get all merchants
+app.get('/api/merchants', (req, res) => {
+	Merchant.findAll().then(merchants => res.json(merchants))
+})
